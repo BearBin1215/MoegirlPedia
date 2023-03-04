21f7ddd36e9f97d444bba6f561a2978a76ee08b2
BearBin <460072919@qq.com>
2023-03-04 21:00:35 +0800

    update

--- editCount.js
+++ editCount.js
@@ -3,6 +3,7 @@ $(() => {
     if (mw.config.get("skin") === "vector") {
         $("#pt-mycontris").append(`(${ mw.config.get("wgUserEditCount") })`);
     }
+    else {
+        $("#moe-user-dropdown");
+    }
 });
-
-$("#moe-user-dropdown");
--- vector-bg.js
+++ vector-bg.js
@@ -1,1 +1,2 @@
-$('body.skin-vector').append('<div class="vector-custom-bg"></div>')
+"use strict";
+$("body.skin-vector").append('<div class="vector-custom-bg"></div>');
