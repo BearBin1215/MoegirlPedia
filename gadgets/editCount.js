7f58f7771a12fdfcc373576a20db469e7aea5b16
BearBin <460072919@qq.com>
2023-04-26 23:41:02 +0800

    .

--- gadgets/editCount.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-$(() => {
-    if (mw.config.get("skin") === "vector") {
-        $("#pt-mycontris a").append(`(${mw.config.get("wgUserEditCount")})`);
-    }
-    else {
-        $("#moe-user-dropdown");
-    }
-});
