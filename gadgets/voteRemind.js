d0ced98ccc8d022a41b8e085551b2efea4283e9c
BearBin1215 <bearbin1215@qq.com>
2023-04-27 10:58:01 +0800

    refactor:

--- gadgets/voteRemind.js
+++ gadgets/voteRemind.js
@@ -1,12 +1,3 @@
-/**
- * @todo 输入检查
- * @todo 发送前确认用户列表
- * @todo 排除投票开始后上任的巡查/管理（考虑通过读取Module:UserGroup/data的历史版本）
- * @todo 人事案根据标题自动检测申请用户组来选取通知的用户组
- * @rights edit、noratelimit
- * @dependencies ["mediawiki.api", "mediawiki.util", "mediawiki.notification", "oojs-ui-core", "ext.gadget.site-lib"]
- */
-
 // eslint-disable-next-line
 var _addText = '{{Documentation|content=<p>本小工具用于在提案和权限变更版快速提醒用户参与投票。</p><p>使用方式：在[[Special:MyPage/common.js|个人js页]]添加如下代码</p><pre class="prettyprint lang-javascript" style="margin-top:0">mw.loader.load("/index.php?title=User:BearBin/js/voteRemind.js&action=raw&ctype=text/javascript");</pre><p>如果您不想收到提醒，请前往[[User:BearBin/js/voteRemind.js/Noremind]]取消订阅。</p><p>如果您有好的建议，欢迎前往[[User_talk:BearBin|我的讨论页]]，或在GitHub上[https://github.com/BearBin1215/MoegirlPedia/issues 提交issue]。</p>}}';
 
@@ -19,37 +10,11 @@ $(() => (async () => {
         const $body = $("body");
         const PAGENAME = mw.config.get("wgPageName");
         const isProposal = mw.config.get("wgTitle").startsWith("提案/讨论中提案/"); // 提案还是人事案
-        const isBot = mw.config.get("wgUserGroups").includes("flood"); // 用户是否拥有机器用户权限
+        const isBot = mw.config.get("wgUserGroups").includes("flood"); // 是否拥有机器用户权限
         const GadgetTitle = wgULS("一键发送投票提醒", "一鍵發送投票提醒");
 
-        // 从[[Module:UserGroup/data]]获取当前各用户组人员列表
-        const userGroup = await api.post({
-            action: "query",
-            titles: "Module:UserGroup/data",
-            prop: "revisions",
-            rvprop: "content",
-            rvlimit: 1,
-        });
-        const userList = JSON.parse(Object.values(userGroup.query.pages)[0].revisions[0]["*"]);
-
-        // 从[[User:BearBin/js/voteRemind.js/Noremind]]获取不愿意收到投票提醒的用户
-        const Noremind = await api.post({
-            action: "query",
-            titles: "User:BearBin/js/voteRemind.js/Noremind",
-            prop: "revisions",
-            rvprop: "content",
-            rvlimit: 1,
-        });
-        const userExcluded = Object.values(Noremind.query.pages)[0].revisions[0]["*"].split(/\n\* */);
-
-        // 获取{{投票}}模板所在讨论串二级标题内.mw-headline的id值
-        const mwHeadlines = $(".votebox").parent(".discussionContainer").children("h2").children(".mw-headline");
-        const headlines = [];
-        for (const item of mwHeadlines) {
-            headlines.push(item.id);
-        }
-
         class ReminderWindow extends OO.ui.ProcessDialog {
+            failList = [];
             static static = {
                 ...super.static,
                 tagName: "div",
@@ -63,7 +28,7 @@ $(() => (async () => {
                     },
                     {
                         action: "submit",
-                        label: wgULS("确认", "確認"),
+                        label: wgULS("发送", "發送"),
                         flags: ["primary", "progressive"],
                     },
                 ],
@@ -90,9 +55,13 @@ $(() => (async () => {
                 });
 
                 // 人事案，选择章节标题
+                this.headlines = [];
+                for (const item of $(".votebox").parent(".discussionContainer").children("h2").children(".mw-headline")) {
+                    this.headlines.push(item.id);
+                }
                 this.sectionTitleDropdown = new OO.ui.DropdownInputWidget({
                     options: [
-                        ...headlines.map((v) => ({
+                        ...this.headlines.map((v) => ({
                             data: v,
                             label: v.replaceAll("_", " "),
                         })),
@@ -103,12 +72,15 @@ $(() => (async () => {
                     align: top,
                 });
 
-                // 人事案，选择要提醒的用户组
+                // 人事案选择要提醒的用户组
+                this.Papp = new OO.ui.RadioOptionWidget({ data: "p", label: "管理员、巡查姬", selected: true }); // 管、监、查、行
+                this.Sapp = new OO.ui.RadioOptionWidget({ data: "s", label: "管理员" }); // 脚
+                this.Iapp = new OO.ui.RadioOptionWidget({ data: "i", label: "管理员、界面管理员" }); // 界
                 this.groupsRadioSelect = new OO.ui.RadioSelectWidget({
                     items: [
-                        new OO.ui.RadioOptionWidget({ data: "p", label: "管理员、巡查姬", selected: true }), // 管、监、查、行
-                        new OO.ui.RadioOptionWidget({ data: "s", label: "管理员" }), // 脚
-                        new OO.ui.RadioOptionWidget({ data: "i", label: "管理员、界面管理员" }), // 界
+                        this.Papp,
+                        this.Sapp,
+                        this.Iapp,
                     ],
                 });
                 const groupsFiled = new OO.ui.FieldLayout(this.groupsRadioSelect, {
@@ -136,23 +108,24 @@ $(() => (async () => {
             get sectionTitle() {
                 return this.sectionTitleDropdown.getValue();
             }
-
             get groupSelected() {
                 return this.groupsRadioSelect.findSelectedItem()?.getData?.();
             }
 
-            // 生成提醒用户列表
-            get usersToVote() {
-                const hrefList = [];
-                const userVoted = [];
-
+            // 获取选择的用户组列表（提案则不选择）
+            async getUserGroup() {
+                const userGroup = await api.post({
+                    action: "query",
+                    titles: "Module:UserGroup/data",
+                    prop: "revisions",
+                    rvprop: "content",
+                    rvlimit: 1,
+                });
+                const userList = JSON.parse(Object.values(userGroup.query.pages)[0].revisions[0]["*"]);
+                
                 let groupsToVote;
                 if (isProposal) {
                     groupsToVote = [...userList.sysop, ...userList.patroller];
-                    // 提案检测.votebox后面的<ol>内的用户链接
-                    $(".votebox ~ ol a[href^='/User'], .votebox ~ ol a[href^='/index.php?title=User']").each(function () {
-                        hrefList.push(this.href);
-                    });
                 } else {
                     switch (this.groupSelected) {
                         case "p":
@@ -164,21 +137,53 @@ $(() => (async () => {
                         case "i":
                             groupsToVote = Array.from(new Set([...userList.sysop, ...userList["interface-admin"]]));
                     }
+                }
+                return groupsToVote;
+            }
+
+            // 获取已投票用户列表
+            get userVoted() {
+                const userVotedList = [];
+                const hrefList = [];
+                if (isProposal) {
+                    // 提案检测.votebox后面的<ol>内的用户链接
+                    $(".votebox ~ ol a[href^='/User'], .votebox ~ ol a[href^='/index.php?title=User']").each(function () {
+                        hrefList.push(this.href);
+                    });
+                } else {
                     // 人事案检测当前选择标题所在.discussionContainer内.votebox后面的ol
                     $(`#${this.sectionTitle}`).parents(".discussionContainer").find(".votebox ~ ol a[href^='/User'], .votebox ~ ol a[href^='/index.php?title=User']").each(function () {
                         hrefList.push(this.href);
                     });
                 }
                 for (const item of hrefList) {
-                    userVoted.push(decodeURI(item.replace(/.*User(_talk)?:([^&]*).*/g, "$2")));
+                    userVotedList.push(decodeURI(item.replace(/.*User(_talk)?:([^&]*).*/g, "$2")));
                 }
+                return userVotedList; // 这个用于后面求差集
+            }
 
-                const setExcluded = new Set([...userExcluded, ...userVoted]);
-                return groupsToVote.filter((x) => !setExcluded.has(x));
+            // 根据选择的用户组，去掉已投票用户和不愿收到提醒用户，得到要提醒的用户列表
+            async getUserToRemind() {
+                let userToRemind;
+                await this.getUserGroup().then((result) => {
+                    userToRemind = result;
+                });
+
+                const Noremind = await api.post({
+                    action: "query",
+                    titles: "User:BearBin/js/voteRemind.js/Noremind",
+                    prop: "revisions",
+                    rvprop: "content",
+                    rvlimit: 1,
+                });
+                const userNoremind = Object.values(Noremind.query.pages)[0].revisions[0]["*"].split(/\n\* */);
+                const userExcluded = new Set([...this.userVoted, ...userNoremind]);
+                console.log(userToRemind.filter((x) => !userExcluded.has(x)));
+                return userToRemind.filter((x) => !userExcluded.has(x));
             }
 
             // 生成链接
-            getLink() {
+            get link() {
                 if (isProposal) {
                     return `[[萌娘百科_talk:提案/讨论中提案/${this.proposalTitle}|${this.proposalTitle.replaceAll("_", " ")}]]`;
                 }
@@ -186,30 +191,24 @@ $(() => (async () => {
             }
 
             // 发送提醒
-            async remind() {
-                const errorList = [];
-                const link = this.getLink();
-                for (const userName of this.usersToVote) {
-                    const d = await api.postWithToken("csrf", {
-                        format: "json",
-                        action: "edit",
-                        section: "new",
-                        watchlist: "nochange",
-                        tags: "Automation tool",
-                        bot: isBot,
-                        title: `User_talk:${userName}`,
-                        sectiontitle: "投票提醒",
-                        text: `<i style="font-size:small">本通知使用一键提醒小工具发出，如出现错误，请联系[[User_talk:BearBin|BearBin]]。若不希望接到此提醒，请在[[User:BearBin/js/voteRemind.js/Noremind|这个页面]]记录您的用户名。</i><br/>您好，${isProposal ? "提案" : "人事案"}【${link}】已经开始投票。您尚未投票，请及时参与喵～——~~~~`,
-                    })
-                        .done(() => {
-                            mw.notify(wgULS(`向用户${userName}发送投票提醒成功。`, `向使用者${userName}發送投票提醒成功。`));
-                        });
-                    if (d.error) {
-                        mw.notify(wgULS(`向${userName}发送投票提醒失败：${d.error.code}。`, `向${userName}發送投票提醒失敗：${d.error.code}。`));
-                        errorList.push(userName);
-                    }
+            async remind(username) {
+                const send = await api.postWithToken("csrf", {
+                    format: "json",
+                    action: "edit",
+                    section: "new",
+                    watchlist: "nochange",
+                    tags: "Automation tool",
+                    bot: isBot,
+                    title: `User_talk:${username}`,
+                    sectiontitle: "投票提醒",
+                    text: `<i style="font-size:small">本通知使用一键提醒小工具发出，如出现错误，请联系[[User_talk:BearBin|BearBin]]。若不希望接到此提醒，请在[[User:BearBin/js/voteRemind.js/Noremind|这个页面]]记录您的用户名。</i><br/>您好，${isProposal ? "提案" : "人事案"}【${this.link}】已经开始投票。您尚未投票，请及时参与喵～——~~~~`,
+                }).done(() => {
+                    mw.notify(wgULS(`向${username}发送投票提醒成功。`, `向${username}發送投票提醒成功。`));
+                });
+                if(send.error) {
+                    this.failList.push(username);
+                    mw.notify(wgULS(`向${username}发送投票提醒失败：${send.error.code}。`, `向${username}發送投票提醒失敗：${send.error.code}。`), { type: "error" });
                 }
-                return errorList;
             }
 
             getActionProcess(action) {
@@ -219,26 +218,25 @@ $(() => (async () => {
                     }, this);
                 } else if (action === "submit") {
                     return new OO.ui.Process($.when((async () => {
-                        try {
-                            await this.remind().then((result) => {
-                                if (result.length > 0) {
-                                    oouiDialog.alert(result.join("、"), {
-                                        title: wgULS("向以下用户发送提醒失败", "向以下使用者發送提醒失敗"),
-                                        size: "small",
-                                    });
-                                }
+                        this.failList = []; // 清空失败列表（真的有必要吗（））
+                        await this.getUserToRemind().then(async (result) => {
+                            for (const user of result) {
+                                await this.remind(user);
+                            }
+                        });
+                        if(this.failList.length > 0) {
+                            oouiDialog.alert(this.failList.join("、"), {
+                                title: wgULS("向以下用户发送提醒失败", "向以下使用者發送提醒失敗"),
+                                size: "small",
                             });
-                            this.close({ action });
-                        } catch (e) {
-                            console.error("OOUI error:", e);
-                            throw new OO.ui.Error(e);
                         }
+                        this.close({ action });
                     })()).promise(), this);
                 }
                 return super.getActionProcess(action);
             }
         }
-
+        
         // 添加按钮和窗口
         const windowManager = new OO.ui.WindowManager();
         $body.append(windowManager.$element);
@@ -248,7 +246,7 @@ $(() => (async () => {
         });
         windowManager.addWindows([reminderDialog]);
 
-        $(mw.util.addPortletLink("p-cactions", "#", "投票提醒", "vote-remind", GadgetTitle, "r")).on("click", (e) => {
+        $(mw.util.addPortletLink("p-cactions", "#", "投票提醒", "ca-vote-remind", GadgetTitle, "r")).on("click", (e) => {
             e.preventDefault();
             windowManager.openWindow(reminderDialog);
             $body.css("overflow", "auto");
