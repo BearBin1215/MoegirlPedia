80eeb6d9ad86b07f8672794235f6ce7aef0f07d1
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-08-16 17:31:05 +0800

    update ListEnhancer.js

--- gadgets/ListEnhancer.js
+++ gadgets/ListEnhancer.js
@@ -1,19 +1,29 @@
+/**
+ * 测试阶段，漏洞很多，慎用
+ */
+
 "use strict";
-if (mw.config.get("wgNamespaceNumber") === -1) {
+$(() => {
+    if (mw.config.get("wgNamespaceNumber") !== -1) {
+        return;
+    }
+
     /**
-     * 复制内容到剪贴板
-     * @param {string} text 要复制的文本
-     * @param {string} selector 选择器，用于替换指定元素的文字内容
+     * 执行复制操作
+     * @param {string} content 要复制的内容
+     * @param {jQuery} $element 要改变文字的元素
+     * @param {string} successText 复制成功显示的文字
+     * @param {string} errorText 复制失败显示的文字
      */
-    const copyAction = (text, selector) => {
-        const $selector = $(selector);
-        navigator.clipboard.writeText(text).then(() => {
-            $selector.text("[复制成功]");
-            setTimeout(() => $selector.text("[复制列表]"), 3000); // 恢复内容
+    const copyAction = (content, $element, successText = "复制成功", errorText = "复制失败") => {
+        const buttonText = $element.text();
+        navigator.clipboard.writeText(content).then(() => {
+            $element.text(successText);
+            setTimeout(() => $element.text(buttonText), 3000); // 恢复内容
         }, (err) => {
-            $selector.text("[复制失败]");
+            $element.text(errorText);
             console.error("复制失败: ", err);
-            setTimeout(() => $selector.text("[复制列表]"), 3000);
+            setTimeout(() => $element.text(buttonText), 3000);
         });
     };
 
@@ -24,8 +34,9 @@ if (mw.config.get("wgNamespaceNumber") === -1) {
             $("#mw-whatlinkshere-list>li>a").each((_, ele) => {
                 linkList.push($(ele).text());
             });
-            copyAction(linkList.join("\n"), e.target);
+            copyAction(linkList.join("\n"), $(e.target), "[复制成功]", "[复制失败]");
         }).css("padding-left", ".6em"));
+        return;
     }
 
     // Special:搜索
@@ -52,19 +63,24 @@ if (mw.config.get("wgNamespaceNumber") === -1) {
         let showDetail = true;
         const linkList = [];
 
-        // 将“多媒体”改为搜索替换内容常的目标名字空间（主、用户、模板、帮助、分类、模块）
-        const $imageSearch = $(".search-types li a").eq(1);
-        $imageSearch
+        // 复制一个按钮插入到多媒体搜索后面，用于搜索要用语替换的页面
+        const $searchMedia = $(".search-types li").eq(1);
+        const $nsToReplace = $searchMedia.clone(false);
+        $nsToReplace.children("a")
             .text("替换空间")
-            .attr("href", $imageSearch.attr("href").replace("profile=images", "profile=advanced&limit=5000&ns0=1&ns2=1&ns10=1&ns12=1&ns14=1&ns828=1"));
+            .attr("title", "主、模板等替换常用名字空间")
+            .attr("href", function () {
+                return this.href.replace("profile=images", "profile=advanced&limit=500&ns0=1&ns2=1&ns6=1&ns10=1&ns12=1&ns14=1&ns828=1"); // 太多了超时，所以500
+            });
+        $nsToReplace.insertAfter($searchMedia);
 
         // 在搜索结果的每个页面后添加编辑按钮，顺带存一个linkList列表用于后续复制列表
-        $(".mw-search-result-heading>a").each((_, ele) => {
+        $("a[data-serp-pos]").each((_, ele) => {
             linkList.push(decodeURIComponent($(ele).attr("href")).replace("/", "").replaceAll("_", " "));
             $(ele).before(`<a class="searchplus-edit" href="${ele.href}?action=edit">[编辑]</a>`);
         });
 
-        // 添加详情开关和复制完整列表按钮
+        // 添加详情开关和复制列表按钮
         const $detailToggle = $("<a>隐藏详情</a>").on("click", (e) => {
             if (showDetail) {
                 $(".searchresult, .mw-search-result-data").hide(); // 隐藏除标题以外的详情
@@ -79,7 +95,7 @@ if (mw.config.get("wgNamespaceNumber") === -1) {
             }
         });
         const $copyList = $("<a>复制列表</a>").on("click", (e) => {
-            copyAction(linkList.join("\n"), e.target);
+            copyAction(linkList.join("\n"), $(e.target));
         });
 
         // 把按钮放到输入条下方的名字空间选择右侧
@@ -90,5 +106,6 @@ if (mw.config.get("wgNamespaceNumber") === -1) {
             $copyList,
             '<span class="mw-editsection-bracket">]</span>',
         ));
+        return;
     }
-}
+});
