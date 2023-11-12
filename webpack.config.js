6d35a9df2de1acab7c505a0ca8e0b901e6e982ee
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-12 12:56:28 +0800

    build(webpack): 使用autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -3,60 +3,72 @@ const glob = require('glob');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: glob.sync('./src/gadgets/**/index.js')
-        .map((filename) => filename
-            .replace(/\\/g, '/')
-            .replace(/^(?:.\/)?(.*)$/, './$1'))
-        .reduce((entries, path) => {
-            const entry = path.replace('./src/gadgets/', '').replace('/index.js', '');
-            entries[entry] = path;
-            return entries;
-        }, {}),
-    output: {
-        filename: '[name].min.js',
-        path: path.resolve(__dirname, 'dist/gadgets'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    },
-                },
-            },
-            {
-                test: /\.m?js$/,
-                resolve: {
-                  fullySpecified: false,
-                },
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader',
+  entry: glob.sync('./src/gadgets/**/index.js')
+    .map((filename) => filename
+      .replace(/\\/g, '/') // windows下会输出反斜杠，需要替换
+      .replace(/^(?:.\/)?(.*)$/, './$1'))
+    .reduce((entries, path) => {
+      const entry = path.replace('./src/gadgets/', '').replace('/index.js', '');
+      entries[entry] = path;
+      return entries;
+    }, {}),
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist/gadgets'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
+                  ],
                 ],
+              },
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
+          },
         ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-            })
-        ]
-    },
-    mode: 'production',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      })
+    ]
+  },
+  mode: 'production',
 };
