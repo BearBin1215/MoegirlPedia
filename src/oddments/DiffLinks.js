5f4ed890a37567e42cc515427ac94c249e19cdce
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-13 21:11:11 +0800

    feat: new oddment

---
+++ src/oddments/DiffLinks.js
@@ -0,0 +1,3 @@
+document.querySelectorAll(".diff-contentalign-left tr:not(.diff-title) div").forEach((ele) => {
+    ele.innerHTML = ele.innerHTML.replace(/\[\[( *)([^{}[\]]+)( *)(\|[^{}[\]]+)?\]\]/g, '[[<a href="/$2" style="color:#042F76">$1$2</a>$3$4]]');
+});
