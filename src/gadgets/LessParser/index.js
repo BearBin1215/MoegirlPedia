3dde1e6a2b49c65308b5af62ddb53ca50fc222bf
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-15 10:52:17 +0800

    feat(LessParser): 支持上传和复制

--- src/gadgets/LessParser/index.js
+++ src/gadgets/LessParser/index.js
@@ -2,14 +2,28 @@ import React, { useState, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { render } from 'less';
+import { copyText } from '../../utils/clipboard';
 import './index.less';
 
 const ParserModal = () => {
     const [isOpen, setOpen] = useState(true);
+    const uploadRef = useRef(null);
     const inputRef = useRef(null);
     const outputRef = useRef(null);
 
     const closeModal = () => setOpen(false);
+
+    const upload = () => {
+        uploadRef.current.click();
+    };
+
+    const onUpload = ({ target: { files: [file] } }) => {
+        const fileReader = new FileReader();
+        fileReader.addEventListener("loadend", () => {
+            inputRef.current.value = fileReader.result;
+        });
+        fileReader.readAsText(file);
+    };
 
     const parseLess = () => {
         render(inputRef.current.value, (err, output) => {
@@ -40,8 +54,17 @@ const ParserModal = () => {
                         <div className='modal-body'>
                             <textarea name='less-input' ref={inputRef} />
                             <div className='button-area'>
+                                <input
+                                    type="file"
+                                    ref={uploadRef}
+                                    onChange={onUpload}
+                                    accept='.less'
+                                    hidden
+                                />
+                                <button onClick={upload}>上传</button>
                                 <button onClick={parseLess}>解析</button>
                                 <button onClick={clear}>清空</button>
+                                <button onClick={() => copyText(outputRef.current.value)}>复制</button>
                             </div>
                             <textarea name='css-output' ref={outputRef} readOnly />
                         </div>
