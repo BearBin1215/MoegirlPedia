100dc1db7853dd8a61600f30ff6439f81f809e6a
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-15 11:05:19 +0800

    chore:

--- src/gadgets/LessParser/index.js
+++ src/gadgets/LessParser/index.js
@@ -6,17 +6,27 @@ import { copyText } from '../../utils/clipboard';
 import './index.less';
 
 const ParserModal = () => {
-    const [isOpen, setOpen] = useState(true);
-    const uploadRef = useRef(null);
-    const inputRef = useRef(null);
-    const outputRef = useRef(null);
+    const [isOpen, setOpen] = useState(true); // 显示状态
+    const uploadRef = useRef(null); // 上传框（隐藏）
+    const inputRef = useRef(null); // 输入框
+    const outputRef = useRef(null); // 输出框
 
+    /**
+     * 关闭弹窗
+     */
     const closeModal = () => setOpen(false);
 
+    /**
+     * 点击上传
+     */
     const upload = () => {
         uploadRef.current.click();
     };
 
+    /**
+     * 上传完毕读取内容
+     * @param {React.ChangeEvent} param0 
+     */
     const onUpload = ({ target: { files: [file] } }) => {
         const fileReader = new FileReader();
         fileReader.addEventListener("loadend", () => {
@@ -25,17 +35,27 @@ const ParserModal = () => {
         fileReader.readAsText(file);
     };
 
+    /**
+     * 执行解析
+     */
     const parseLess = () => {
         render(inputRef.current.value, (err, output) => {
             outputRef.current.value = err ? err.message : output.css;
         });
     };
 
+    /**
+     * 清空输入输出
+     */
     const clear = () => {
+        uploadRef.current.value = '';
         inputRef.current.value = '';
         outputRef.current.value = '';
     };
 
+    /**
+     * 渲染
+     */
     if (isOpen) {
         return (
             <>
@@ -76,6 +96,9 @@ const ParserModal = () => {
     return null;
 };
 
+/**
+ * 添加入口
+ */
 mw.loader.using("mediawiki.util").then(() => {
     mw.util.addPortletLink("p-tb", "javascript:void(0)", "Less解析器", "t-lessparser").addEventListener("click", () => {
         createRoot(document.createElement('div')).render(<ParserModal />);
