907a4632121a5118a1d037be303ae8a52181a07c
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-16 17:16:46 +0800

    feat(ColorPicker): new gadget

---
+++ src/gadgets/ColorPicker/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { SketchPicker } from 'react-color';
+import { Button } from '../../components';
+import { copyText } from '../../utils/clipboard';
+import './index.less';
+
+class PickerModal extends Component {
+    state = {
+        displayPicker: true,
+        color: '#222',
+    };
+
+    constructor(props) {
+        super(props);
+        this.setState({ displayPicker: props.open });
+    }
+
+    handleChange = (color) => {
+        this.setState({ color: color.hex });
+    };
+
+    handleClose = () => {
+        this.setState({ displayPicker: false });
+    };
+
+    handleCopy = () => {
+        copyText(this.state.color);
+    };
+
+    render() {
+        return (
+            <>
+                {createPortal(
+                    this.state.displayPicker ? <div id='color-picker'>
+                        <SketchPicker
+                            color={this.state.color}
+                            onChange={this.handleChange}
+                        />
+                        <div className='button-zone'>
+                            <Button onClick={this.handleCopy}>复制</Button>
+                            <Button type='danger' onClick={this.handleClose}>关闭</Button>
+                        </div>
+                    </div> : null,
+                    document.body,
+                )}
+            </>
+        );
+    }
+}
+
+mw.loader.using("mediawiki.util").then(() => {
+    mw.util.addPortletLink("p-tb", "javascript:void(0)", "颜色选择器", "t-colorpicker")
+        .addEventListener("click", () => {
+            if (!document.getElementById('color-picker')) {
+                createRoot(document.createElement('div')).render(<PickerModal open />);
+            }
+        });
+});
