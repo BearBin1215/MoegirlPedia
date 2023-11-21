07ed8f6cda8d4df9d60f2e6ae5f7ad4a6472474d
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-21 21:52:38 +0800

    feat

--- dist/oddments/DiffLinks.js
+++ dist/oddments/DiffLinks.js
@@ -1,3 +1,10 @@
 document.querySelectorAll(".diff-contentalign-left tr:not(.diff-title) div").forEach(function (ele) {
-    ele.innerHTML = ele.innerHTML.replace(/\[\[( *)([^{}[\]<>]+)( *)(\|[^{}[\]]+)?\]\]/g, '[[<a href="/$2" style="color:#042F76">$1$2</a>$3$4]]');
+    ele.innerHTML = ele.innerHTML.replace(/\[\[([^\]|{<>]+)(?:\|([^\]]+))?\]\]/g, function (match, pageName, displayName) {
+        var link = "<a href=\"/".concat(encodeURIComponent(pageName), "\" style=\"color:#042F76\">").concat(pageName, "</a>");
+        if (displayName) {
+            link = "".concat(link, "|").concat(displayName);
+        }
+        link = "[[".concat(link, "]]");
+        return link;
+    });
 });
--- src/oddments/DiffLinks.js
+++ src/oddments/DiffLinks.js
@@ -1,3 +1,10 @@
 document.querySelectorAll(".diff-contentalign-left tr:not(.diff-title) div").forEach((ele) => {
-    ele.innerHTML = ele.innerHTML.replace(/\[\[( *)([^{}[\]<>]+)( *)(\|[^{}[\]]+)?\]\]/g, '[[<a href="/$2" style="color:#042F76">$1$2</a>$3$4]]');
+    ele.innerHTML = ele.innerHTML.replace(/\[\[([^\]|{<>]+)(?:\|([^\]]+))?\]\]/g, (match, pageName, displayName) => {
+        let link = `<a href="/${encodeURIComponent(pageName)}" style="color:#042F76">${pageName}</a>`;
+        if (displayName) {
+            link = `${link}|${displayName}`;
+        }
+        link = `[[${link}]]`;
+        return link;
+    });
 });
