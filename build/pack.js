d5eafb67c0445bca3223afcd6b3ab801432f8877
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-23 22:06:01 +0800

    build: 支持ts

--- build/pack.js
+++ build/pack.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process');
 
+// eslint-disable-next-line prefer-destructuring
 const gadgetName = process.argv[2];
 console.log(`开始打包${gadgetName || '所有小工具'}……`);
 exec(
--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,11 +1,11 @@
 const glob = require('glob');
 
-const entry = (process.env.gadgetname ? [`./src/gadgets/${process.env.gadgetname}/index.js`] : glob.sync('./src/gadgets/**/index.js'))
+const entry = glob.sync(process.env.gadgetname ? `./src/gadgets/${process.env.gadgetname}/index.{js,jsx,ts,tsx}` : './src/gadgets/**/index.{js,jsx,ts,tsx}')
   .map((filename) => filename
     .replace(/\\/g, '/') // windows下会输出反斜杠，需要替换
     .replace(/^(?:.\/)?(.*)$/, './$1'))
   .reduce((entries, path) => {
-    const entry = path.replace('./src/gadgets/', '').replace('/index.js', '');
+    const entry = path.replace('./src/gadgets/', '').replace(/\/index\.(js|jsx|ts|tsx)$/, '');
     entries[entry] = path;
     return entries;
   }, {});
@@ -30,6 +30,16 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.webpack.json',
+          },
+        },
+        exclude: /node_modules/,
       },
       {
         test: /\.m?js$/,
