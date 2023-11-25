cb20b9e7fe22d5f594e48037058e6517eea9bfe9
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-25 18:56:03 +0800

    feat: components

--- src/components/Loger/React/index.js
+++ src/components/Loger/React/index.js
@@ -5,6 +5,8 @@ import './index.less';
  * 日志组件
  */
 export default class Loger extends Component {
+    state = {};
+
     /**
      * 定义筛选按钮
      */
@@ -26,7 +28,7 @@ export default class Loger extends Component {
         },
     };
 
-    LogerData = {
+    logerData = {
         detail: [],
         count: {},
     };
@@ -35,18 +37,28 @@ export default class Loger extends Component {
      * 清空
      */
     clear = () => {
-        this.LogerData.detail.length = 0;
-        for (const key in this.LogerData.count) {
-            this.LogerData.count[key] = 0;
+        if (this.logerData.clear) {
+            this.logerData.clear();
+        } else {
+            this.logerData.detail.length = 0;
+            for (const key in this.logerData.count) {
+                this.logerData.count[key] = 0;
+            }
         }
     };
 
     constructor(props) {
         super(props);
-        if (props.filterButtons) {
+        if (props.records) {
+            this.logerData = props.records;
+            this.filterButtons = props.records.types;
+        } else if (props.filterButtons) {
+            this.logerData = props.value;
             this.filterButtons = props.filterButtons;
         }
-        this.LogerData = props.value;
+        for (const type in this.filterButtons) {
+            this.state[type] = true;
+        }
     }
 
     render() {
@@ -62,19 +74,34 @@ export default class Loger extends Component {
                     <nav className='loger-filter'>
                         {Object.entries(this.filterButtons).map(([name, { color, icon, text }]) => {
                             return (
-                                <button key={name} style={{ color }}>
+                                <button
+                                    className='loger-filter-selected'
+                                    key={name}
+                                    style={{ color }}
+                                    onClick={({ currentTarget }) => {
+                                        currentTarget.classList.toggle('loger-filter-selected');
+                                        this.setState({
+                                            [name]: !this.state[name],
+                                        });
+                                    }}
+                                >
                                     <span className='loger-filter-icon'>{icon}</span>
-                                    <span className='loger-filter-count'>{this.LogerData.count[name]}</span>
+                                    <span className='loger-filter-count'>{this.logerData.count[name]}</span>
                                     {` ${text}`}
                                 </button>
                             );
                         })}
                     </nav>
                     <ul className='loger-lines'>
-                        {this.LogerData.detail.map(({ id, type, text }) => {
-                            console.log(id, type, text);
+                        {this.logerData.detail.map(({ id, type, text }) => {
                             return (
-                                <li key={id} style={{ color: this.filterButtons[type].color }}>
+                                <li
+                                    key={id}
+                                    style={{
+                                        color: this.filterButtons[type].color,
+                                        display: this.state[type] ? '' : 'none',
+                                    }}
+                                >
                                     {text}
                                 </li>
                             );
--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,4 @@ export { default as Button } from './Button';
  * 可能用的较大组件
  */
 export { default as Loger } from './Loger/React';
+export { default as Records } from './Loger/React/Record';
