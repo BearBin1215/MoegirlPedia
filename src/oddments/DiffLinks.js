4d87a92cb094db3b5c0e5c8c17a2864e89fd0fa5
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-11-26 14:19:45 +0800

    chore

--- src/oddments/DiffLinks.js
+++ src/oddments/DiffLinks.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".diff-contentalign-left tr:not(.diff-title) div").forEach((ele) => {
     ele.innerHTML = ele.innerHTML.replace(/\[\[([^\]|{<>]+)(?:\|([^\]]+))?\]\]/g, (match, pageName, displayName) => {
-        let link = `<a href="/${encodeURIComponent(pageName.replace(/&nbsp;/, "_"))}" style="color:#042F76">${pageName}</a>`;
+        let link = `<a href="/${encodeURIComponent(pageName.replace(/&nbsp;/g, "_"))}" style="color:#042F76">${pageName}</a>`;
         if (displayName) {
             link = `${link}|${displayName}`;
         }
