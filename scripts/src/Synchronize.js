48b4809f72018781ba617f84030dc4c44b17457d
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-12-08 11:44:12 +0800

    ci: fix

--- scripts/src/Synchronize.js
+++ scripts/src/Synchronize.js
@@ -4,7 +4,7 @@ import config from "./config.js";
 import { exec } from "child_process";
 
 // 获取最近一次提交变更的文件
-exec('git diff --name-only HEAD HEAD~1', (error, stdout) => {
+exec('git diff --name-only HEAD $(git rev-parse HEAD^)', (error, stdout) => {
     if (error) {
         console.error(`获取最近提交文件失败：${error}`);
         return;
