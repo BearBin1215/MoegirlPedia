0cb8005681704fdc7209347bf737efbd4e6ec57d
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-12-25 21:53:20 +0800

    feat(TableExporter): 新工具

---
+++ src/gadgets/TableExporter/index.js
@@ -0,0 +1,19 @@
+import { 
+    utils,
+    writeFile,
+} from "xlsx";
+import './index.less';
+
+document.querySelectorAll("table.wikitable").forEach((table) => {
+    const exportButton = document.createElement("div");
+    exportButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17 12v5H3v-5H1v5a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5z"/><path d="M15 9h-4V1H9v8H5l5 6z"/></svg>';
+    exportButton.classList.add("export-table-button");
+    exportButton.title = "导出表格";
+
+    exportButton.addEventListener("click", () => {
+        const workbook = utils.table_to_book(table);
+        writeFile(workbook, "export.xlsx");
+    });
+
+    table.append(exportButton);
+});
