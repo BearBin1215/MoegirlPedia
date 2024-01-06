fc4fc67476ea3c25c9488cfb1bdc1410b4497ef1
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-01-06 20:37:34 +0800

    feat(ListEnhancer): 支持分类页

--- src/gadgets/ListEnhancer/index.js
+++ src/gadgets/ListEnhancer/index.js
@@ -1,8 +1,6 @@
-/**
- * 测试阶段，慎用
- */
+import { categoryMembers } from "../../utils/apiList";
 
-if (mw.config.get("wgNamespaceNumber") === -1) {
+mw.loader.using(['mediawiki.notification', 'mediawiki.api']).done(() => {
     /**
      * 执行复制操作
      * @param {string} content 要复制的内容
@@ -10,106 +8,156 @@ if (mw.config.get("wgNamespaceNumber") === -1) {
      * @param {string} successText 复制成功显示的文字
      * @param {string} errorText 复制失败显示的文字
      */
-    const copyAction = (content, $element, successText = "复制成功", errorText = "复制失败") => {
+    const copyAction = (content, $element, successText = '复制成功', errorText = '复制失败') => {
         const buttonText = $element.text();
         navigator.clipboard.writeText(content).then(() => {
             $element.text(successText);
             setTimeout(() => $element.text(buttonText), 3000); // 恢复内容
         }, (err) => {
             $element.text(errorText);
-            console.error("复制失败: ", err);
+            mw.notify(`复制失败: ${err}`, {
+                type: 'error',
+                autoHideSeconds: 'long',
+            });
             setTimeout(() => $element.text(buttonText), 3000);
         });
     };
 
-    // Special:链入页面
-    const linkshereEnhance = () => {
-        $("#mw-content-text>p>strong").after($("<a>[复制列表]</a>").on("click", (e) => {
+    if (mw.config.get('wgNamespaceNumber') === -1) {
+        // Special:链入页面
+        const linkshereEnhance = () => {
+            $('#mw-content-text>p>strong').after($('<a>[复制列表]</a>').on('click', ({ target }) => {
+                const linkList = [];
+                $('#mw-whatlinkshere-list>li>a').each((_, ele) => {
+                    linkList.push($(ele).text());
+                });
+                copyAction(linkList.join('\n'), $(target), '[复制成功]', '[复制失败]');
+            }).css('padding-left', '.6em'));
+        };
+
+        // Special:搜索
+        const searchEnhance = () => {
+            mw.loader.addStyleTag('.search-types{display:flex;float:none;align-items:center;}#bearbintools-searchplus{padding:.5em;user-select:none;}#bearbintools-searchplus a{display:inline;padding:0;}.searchplus-edit{margin-left:.5em;float:right;}');
+            let showDetail = true;
             const linkList = [];
-            $("#mw-whatlinkshere-list>li>a").each((_, ele) => {
-                linkList.push($(ele).text());
+
+            // 复制一个按钮插入到多媒体搜索后面，用于搜索要用于替换的页面
+            const $searchMedia = $('.search-types li').eq(1);
+            const $nsToReplace = $searchMedia.clone(false);
+            $nsToReplace.children('a')
+                .text('替换空间')
+                .attr('title', '主、模板等替换常用名字空间')
+                .attr('href', (_, attr) => attr.replace('profile=images', 'profile=advanced&limit=500&ns0=1&ns2=1&ns6=1&ns10=1&ns12=1&ns14=1&ns828=1')); // 太多了超时，所以500
+            $nsToReplace.insertAfter($searchMedia);
+
+            // 在搜索结果的每个页面后添加编辑按钮，顺带存一个linkList列表用于后续复制列表
+            $('a[data-serp-pos]').each((_, ele) => {
+                linkList.push(decodeURIComponent($(ele).attr('href')).replace('/', '').replaceAll('_', ' '));
+                $(ele).before(`<a class="searchplus-edit" href="${ele.href}?action=edit">[编辑]</a>`);
             });
-            copyAction(linkList.join("\n"), $(e.target), "[复制成功]", "[复制失败]");
-        }).css("padding-left", ".6em"));
-    };
 
-    // Special:搜索
-    const searchEnhance = () => {
-        mw.loader.addStyleTag(`
-        .search-types {
-            display: flex;
-            float: none;
-            align-items: center;
+            // 添加详情开关和复制列表按钮
+            const $detailToggle = $('<a>隐藏详情</a>').on('click', (e) => {
+                if (showDetail) {
+                    $('.searchresult, .mw-search-result-data').hide(); // 隐藏除标题以外的详情
+                    $('.mw-search-results li').css('padding-bottom', '0.1em'); // 间距很大，需要控制
+                    $(e.target).text('显示详情');
+                    showDetail = false;
+                } else {
+                    $('.searchresult, .mw-search-result-data').show();
+                    $('.mw-search-results li').css('padding-bottom', '');
+                    $(e.target).text('隐藏详情');
+                    showDetail = true;
+                }
+            });
+            const $copyList = $('<a>复制列表</a>').on('click', (e) => {
+                copyAction(linkList.join('\n'), $(e.target));
+            });
+
+            // 把按钮放到输入条下方的名字空间选择右侧
+            $('#search .search-types, #powersearch .search-types').append($('<div id="bearbintools-searchplus"></div>').append(
+                '<span class="mw-editsection-bracket">[</span>',
+                $detailToggle,
+                '<span class="mw-editsection-divider"> | </span>',
+                $copyList,
+                '<span class="mw-editsection-bracket">]</span>',
+            ));
+
+            // 把下方翻页条复制一份到上面
+            $('.mw-search-pager-bottom').clone().prependTo($('.searchresults'));
+        };
+
+        // 根据特殊页面进行显示
+        switch (mw.config.get('wgCanonicalSpecialPageName')) {
+            case "Whatlinkshere":
+                linkshereEnhance();
+                break;
+            case "Search":
+                searchEnhance();
+                break;
         }
-        #bearbintools-searchplus {
-            padding: .5em;
-            user-select: none;
-        }
-        #bearbintools-searchplus a {
-            display: inline;
-            padding: 0;
-        }
-        .searchplus-edit {
-            margin-left: .5em;
-            float: right;
-        }
-        `);
-        let showDetail = true;
-        const linkList = [];
+    } else if (mw.config.get('wgNamespaceNumber') === 14) {
+        const $subCategories = $('#mw-subcategories'); // 子分类
+        const $categoryMembers = $('#mw-pages'); // 分类内页面
+        const $categoryFiles = $('#mw-category-media'); // 分类内文件
+        $subCategories.find('h2').append(
+            $('<span class="mw-editsection"></span>').append(
+                $('<span class="mw-editsection-bracket">[</span>'),
+                $('<a>复制本页</a>')
+                    .on('click', ({ target }) => {
+                        copyAction($subCategories.find('li a').map((_, ele) => `Category:${$(ele).text()}`).get().join("\n"), $(target));
+                    }),
+                $subCategories.children('a').length
+                    ? $('<span class="mw-editsection-divider"> | </span>')
+                    : null,
+                $subCategories.children('a').length
+                    ? $('<a>复制全部</a>').on('click', async ({ target }) => {
+                        const pageList = await categoryMembers(mw.config.get('wgPageName'), ['subcat']);
+                        copyAction(pageList.join('\n'), $(target));
+                    })
+                    : null,
+                $('<span class="mw-editsection-bracket">]</span>'),
+            ),
+        );
 
-        // 复制一个按钮插入到多媒体搜索后面，用于搜索要用于替换的页面
-        const $searchMedia = $(".search-types li").eq(1);
-        const $nsToReplace = $searchMedia.clone(false);
-        $nsToReplace.children("a")
-            .text("替换空间")
-            .attr("title", "主、模板等替换常用名字空间")
-            .attr("href", (_, attr) => attr.replace("profile=images", "profile=advanced&limit=500&ns0=1&ns2=1&ns6=1&ns10=1&ns12=1&ns14=1&ns828=1")); // 太多了超时，所以500
-        $nsToReplace.insertAfter($searchMedia);
+        $categoryMembers.find('h2').append(
+            $('<span class="mw-editsection"></span>').append(
+                $('<span class="mw-editsection-bracket">[</span>'),
+                $('<a>复制本页</a>')
+                    .on('click', ({ target }) => {
+                        copyAction($categoryMembers.find('li a').map((_, ele) => $(ele).text()).get().join("\n"), $(target));
+                    }),
+                $categoryMembers.children('a').length
+                    ? $('<span class="mw-editsection-divider"> | </span>')
+                    : null,
+                $categoryMembers.children('a').length
+                    ? $('<a>复制全部</a>').on('click', async ({ target }) => {
+                        const pageList = await categoryMembers(mw.config.get('wgPageName'), ['page']);
+                        copyAction(pageList.join('\n'), $(target));
+                    })
+                    : null,
+                $('<span class="mw-editsection-bracket">]</span>'),
+            ),
+        );
 
-        // 在搜索结果的每个页面后添加编辑按钮，顺带存一个linkList列表用于后续复制列表
-        $("a[data-serp-pos]").each((_, ele) => {
-            linkList.push(decodeURIComponent($(ele).attr("href")).replace("/", "").replaceAll("_", " "));
-            $(ele).before(`<a class="searchplus-edit" href="${ele.href}?action=edit">[编辑]</a>`);
-        });
-
-        // 添加详情开关和复制列表按钮
-        const $detailToggle = $("<a>隐藏详情</a>").on("click", (e) => {
-            if (showDetail) {
-                $(".searchresult, .mw-search-result-data").hide(); // 隐藏除标题以外的详情
-                $(".mw-search-results li").css("padding-bottom", "0.1em"); // 间距很大，需要控制
-                $(e.target).text("显示详情");
-                showDetail = false;
-            } else {
-                $(".searchresult, .mw-search-result-data").show();
-                $(".mw-search-results li").css("padding-bottom", "");
-                $(e.target).text("隐藏详情");
-                showDetail = true;
-            }
-        });
-        const $copyList = $("<a>复制列表</a>").on("click", (e) => {
-            copyAction(linkList.join("\n"), $(e.target));
-        });
-
-        // 把按钮放到输入条下方的名字空间选择右侧
-        $("#search .search-types, #powersearch .search-types").append($('<div id="bearbintools-searchplus"></div>').append(
-            '<span class="mw-editsection-bracket">[</span>',
-            $detailToggle,
-            '<span class="mw-editsection-divider"> | </span>',
-            $copyList,
-            '<span class="mw-editsection-bracket">]</span>',
-        ));
-
-        // 把下方翻页条复制一份到上面
-        $(".mw-search-pager-bottom").clone().prependTo($(".searchresults"));
-    };
-
-    // 根据特殊页面进行显示
-    switch (mw.config.get("wgCanonicalSpecialPageName")) {
-        case "Whatlinkshere":
-            linkshereEnhance();
-            break;
-        case "Search":
-            searchEnhance();
-            break;
+        $categoryFiles.find('h2').append(
+            $('<span class="mw-editsection"></span>').append(
+                $('<span class="mw-editsection-bracket">[</span>'),
+                $('<a>复制本页</a>')
+                    .on('click', ({ target }) => {
+                        copyAction($categoryFiles.find('li a.galleryfilename').map((_, ele) => `File:${$(ele).text()}`).get().join("\n"), $(target));
+                    }),
+                $categoryFiles.children('a').length
+                    ? $('<span class="mw-editsection-divider"> | </span>')
+                    : null,
+                $categoryFiles.children('a').length
+                    ? $('<a>复制全部</a>').on('click', async ({ target }) => {
+                        const pageList = await categoryMembers(mw.config.get('wgPageName'), ['file']);
+                        copyAction(pageList.join('\n'), $(target));
+                    })
+                    : null,
+                $('<span class="mw-editsection-bracket">]</span>'),
+            ),
+        );
     }
-}
+});
--- src/utils/apiList/categoryMembers.js
+++ src/utils/apiList/categoryMembers.js
@@ -1,10 +1,11 @@
 /**
  * 获取分类成员，有权限用户使用api，无权限用户使用ajax。
  * 
- * @param {string} category 
+ * @param {string} category 分类名
+ * @param {string[]} cmtype 获取类型：page, subcat, file
  * @returns 
  */
-const getCategoryMembers = async (category) => {
+const getCategoryMembers = async (category, cmtype = ['page', 'subcat', 'file']) => {
     const api = new mw.Api();
     const pageList = [];
     // 有api权限的用户通过API获取，无权限用户通过ajax获取
@@ -16,6 +17,7 @@ const getCategoryMembers = async (category) => {
                 list: "categorymembers",
                 cmlimit: "max",
                 cmtitle: category,
+                cmtype: cmtype.join('|'),
                 cmcontinue,
             });
             if (result.query.categorymembers[0]) {
@@ -28,25 +30,49 @@ const getCategoryMembers = async (category) => {
     } else {
         const getCategoryMembersByAjax = async (link) => {
             const ajaxResult = await $.ajax(link);
+            const selector = cmtype.map((type) => {
+                switch (type) {
+                    case 'page':
+                        return '#mw-pages li a';
+                    case 'subcat':
+                        return '#mw-subcategories li a';
+                    case 'file':
+                        return '#mw-category-media li a.galleryfilename';
+                }
+            }).join(',');
             // 将分类内的页面加入列表
-            const members = $(ajaxResult).find('li a').map((_, ele) => {
+            const members = $(ajaxResult).find(selector).map((_, ele) => {
                 if (ele.classList.contains('CategoryTreeLabel')) {
                     return `Category:${$(ele).text()}`;
+                } else if (ele.classList.contains('galleryfilename')) {
+                    return `File:${$(ele).text()}`;
                 }
                 return $(ele).text();
             }).get();
             pageList.push(...members);
 
             // 获取下一页分类内页面
-            const $pageContinueLink = $(ajaxResult).find('a[href*="&pagefrom="]');
-            if ($pageContinueLink.length > 0) {
-                await getCategoryMembersByAjax($pageContinueLink.eq(0).attr('href'));
+            if (cmtype.includes('page')) {
+                const $pageContinueLink = $(ajaxResult).find('a[href*="&pagefrom="]');
+                if ($pageContinueLink.length > 0) {
+                    await getCategoryMembersByAjax($pageContinueLink.eq(0).attr('href'));
+                }
             }
 
-            // 获取下一页子分类
-            const $catContinueLink = $(ajaxResult).find('a[href*="&subcatfrom="]');
-            if ($catContinueLink.length > 0) {
-                await getCategoryMembersByAjax($catContinueLink.eq(0).attr('href'));
+            if (cmtype.includes('subcat')) {
+                // 获取下一页子分类
+                const $catContinueLink = $(ajaxResult).find('a[href*="&subcatfrom="]');
+                if ($catContinueLink.length > 0) {
+                    await getCategoryMembersByAjax($catContinueLink.eq(0).attr('href'));
+                }
+            }
+
+            if (cmtype.includes('file')) {
+                // 获取下一页分类文件
+                const $catContinueLink = $(ajaxResult).find('a[href*="&filefrom="]');
+                if ($catContinueLink.length > 0) {
+                    await getCategoryMembersByAjax($catContinueLink.eq(0).attr('href'));
+                }
             }
         };
         await getCategoryMembersByAjax(`/${category}?action=render`);
