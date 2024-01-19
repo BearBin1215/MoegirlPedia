104e182adf795da57fe4d8e1207f9c7d5d11f4cf
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-01-19 10:06:01 +0800

    feat: 将【编辑自旧版本】功能并入ListEnhancer

--- src/gadgets/ListEnhancer/index.js
+++ src/gadgets/ListEnhancer/index.js
@@ -171,4 +171,17 @@ mw.loader.using(['mediawiki.notification', 'mediawiki.api']).done(() => {
     // 分类内文件
     addCopyButton($('#mw-category-media'), 'file', 'File:', 'li a.galleryfilename');
   }
+  if (mw.config.get('wgAction') === 'history' && document.getElementById('ca-edit')) {
+    $('#pagehistory>li:not(:first-child)').each((num, ele) => {
+      const oldid = $(ele).attr('data-mw-revid');
+      const wgScript = mw.config.get('wgScript');
+      $(ele)
+        .children('.mw-history-undo')
+        .children('a:first-child')
+        .after(` | <a title="编辑自此版本" href="${wgScript}?action=edit&oldid=${oldid}&summary=编辑自[[Special:Permanentlink/${oldid}|版本${oldid}]]">编辑</a>`);
+      if (num === $('#pagehistory>li:not(:first-child)').length - 1) {
+        $(ele).append(`（<span class="mw-history-undo"><a title="编辑自此版本" href="${wgScript}?action=edit&oldid=${oldid}&summary=编辑自[[Special:Permanentlink/${oldid}|版本${oldid}]]">编辑</a></span>）`);
+      }
+    });
+  }
 });
