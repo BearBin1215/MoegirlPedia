8612a795b1307c995999e97fd6b0fded3b825127
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-01-26 16:44:14 +0800

    chore:

--- build/pack.js
+++ build/pack.js
@@ -4,8 +4,8 @@ const gadgetName = process.argv.slice(2);
 
 exec(
   gadgetName
-    ? `cross-env GADGETNAME=${gadgetName.join(',')} webpack --config build/webpack.prod.js`
-    : 'webpack --config build/webpack.prod.js'
+    ? `cross-env GADGETNAME=${gadgetName.join(',')} webpack --config build/webpack.prod.js --mode production`
+    : 'webpack --config build/webpack.prod.js --mode production'
   , (error, stdout) => {
     if (error) {
       console.error(`打包出错: ${error}`);
