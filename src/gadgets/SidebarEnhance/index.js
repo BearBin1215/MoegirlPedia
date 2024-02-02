217047fac336f699734a588b6a4065e52b73196c
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-03 00:09:07 +0800

    feat:

--- src/gadgets/SidebarEnhance/index.js
+++ src/gadgets/SidebarEnhance/index.js
@@ -38,6 +38,9 @@ $(() => {
       addFold($('#moe-custom-sidenav-block h2'), $('#moe-custom-sidenav-block-list'), folderIcon.clone()); // 自定义工具栏
       addFold($('#side-toc-container h3'), $('#side-toc-container > .moe-table-of-contents'), folderIcon.clone()); // 目录
       addFold($('#p-sl h3'), $('#p-sl > div > div'), folderIcon.clone()); // 短连接
+
+      // 将工具放到sticky容器
+      $(".moe-siderail-sticky").prepend($("#moe-custom-sidenav-block"));
       break;
     case 'vector':
     default:
