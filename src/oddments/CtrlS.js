45a358ef53641ceb8458354ff28d735ee1fe48b2
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-03 17:28:52 +0800

    feat(CtrlS): shift小编辑

--- src/oddments/CtrlS.js
+++ src/oddments/CtrlS.js
@@ -1,7 +1,10 @@
 if (mw.config.get('wgAction') === 'edit') {
   window.addEventListener('keydown', function (event) {
-    if (event.ctrlKey && event.key === 's') {
+    if (event.ctrlKey && event.key.toLowerCase() === 's') {
       event.preventDefault();
+      if (event.shiftKey) {
+        document.getElementById('wpMinoredit').click();
+      }
       document.getElementById('wpSave').click();
     }
   });
