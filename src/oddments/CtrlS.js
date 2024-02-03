5999aaac9427497f14e1110ec53c178bf67e36b5
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-03 17:54:31 +0800

    feat:

--- src/oddments/CtrlS.js
+++ src/oddments/CtrlS.js
@@ -1,4 +1,5 @@
 if (mw.config.get('wgAction') === 'edit') {
+  // 编辑页Ctrl+S保存，Ctrl+Shift+S小编辑保存
   window.addEventListener('keydown', function (event) {
     if (event.ctrlKey && event.key.toLowerCase() === 's') {
       event.preventDefault();
@@ -8,4 +9,13 @@ if (mw.config.get('wgAction') === 'edit') {
       document.getElementById('wpSave').click();
     }
   });
+
+  // 在编辑框中时按tab跳转到摘要输入框而非快捷插入
+  var itv = setInterval(function () {
+    console.log(1);
+    if (document.getElementsByClassName('cm-lineWrapping')[0]) {
+      document.getElementsByClassName('cm-lineWrapping')[0].tabIndex = 1;
+      clearInterval(itv);
+    }
+  }, 1000);
 }
