28268c95721554170e177b7820b062feeb668eeb
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-04 10:08:17 +0800

    build(webpack): 优化开发模式输出

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -4,8 +4,13 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
   output: {
     filename: '[name].js',
     path: resolve(__dirname, '../dist/dev'),
+  },
+  watchOptions: {
+    ignored: /node_modules/, // 忽略不需要监听变更的目录
+    aggregateTimeout: 1000, // 防止重复保存时频繁重建
   },
 });
