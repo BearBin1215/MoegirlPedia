28d946a9e50b571d0b2d34db18052bc37593467f
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-14 11:20:57 +0800

    feat(utils): 支持名字空间筛选

--- src/utils/api/includeList.js
+++ src/utils/api/includeList.js
@@ -1,13 +1,24 @@
 /**
  * 获取嵌入了指定页面的页面列表
  * @param {string} pagename 页面名
+ * @param {string} tinamespace 命名空间，格式同api.php中以|分隔
  * @returns {Promise<string[]>} 页面列表
  */
-const includeList = async (pagename) => {
+const includeList = async (pagename, tinamespace = '') => {
   const api = new mw.Api();
   let ticontinue = 1;
   const pageList = [];
   while (ticontinue) {
+    const postBody = {
+      action: 'query',
+      prop: 'transcludedin',
+      titles: pagename,
+      tilimit: 'max',
+      ticontinue,
+    };
+    if (tinamespace) {
+      postBody.tinamespace = tinamespace;
+    }
     const res = await api.post({
       action: 'query',
       prop: 'transcludedin',
--- src/utils/api/linkList.js
+++ src/utils/api/linkList.js
@@ -1,20 +1,25 @@
 /**
  * 获取链接到指定页面的列表
  * @param {string} pagename 页面名
+ * @param {string} lhnamespace 命名空间，格式同api.php中以|分隔
  * @returns {Promise<string[]>} 列表
  */
-const linkList = async (pagename) => {
+const linkList = async (pagename, lhnamespace = '') => {
   const api = new mw.Api();
   let lhcontinue = 1;
   const pageList = [];
   while (lhcontinue) {
-    const res = await api.post({
+    const postBody = {
       action: 'query',
       prop: 'linkshere',
       titles: pagename,
       lhlimit: 'max',
       lhcontinue,
-    });
+    };
+    if (lhnamespace) {
+      postBody.lhnamespace = lhnamespace;
+    }
+    const res = await api.post(postBody);
     if (Object.values(res.query.pages)[0].linkshere) {
       for (const { title } of Object.values(res.query.pages)[0].linkshere) {
         pageList.push(title);
