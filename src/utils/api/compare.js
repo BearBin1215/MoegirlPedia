b93e10e15c4de9cec81dbcaa6ce12551c9c2ab16
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-02-25 14:36:03 +0800

    feat:

--- src/utils/api/compare.js
+++ src/utils/api/compare.js
@@ -1,4 +1,11 @@
-const compare = async (fromtext, totext) => {
+/**
+ * 比较差异
+ * @param {string} fromtext 旧版本
+ * @param {string} totext 新版本
+ * @param {boolean} showTitle 是否显示标题（旧版本、新版本）
+ * @returns {string}
+ */
+const compare = async (fromtext, totext, showTitle = false) => {
   const api = new mw.Api();
   const res = await api.post({
     action: 'compare',
@@ -7,7 +14,10 @@ const compare = async (fromtext, totext) => {
     topst: true,
     fromtitle: 'PAGENAME',
   });
-  return `<table class="diff diff-contentalign-left"><colgroup><col class="diff-marker"><col class="diff-content"><col class="diff-marker"><col class="diff-content"></colgroup><tbody>${res.compare['*']}</tbody></table>`;
+  const result = res.compare['*'];
+  const diffTitle = `<tr class="diff-title"><td colspan="${result ? 2 : 1}" class="diff-otitle">旧版本</td><td colspan="${result ? 2 : 1}" class="diff-ntitle">新版本</td></tr>`;
+  const diffMarker = '<colgroup><col class="diff-marker"><col class="diff-content"><col class="diff-marker"><col class="diff-content"></colgroup>';
+  return `<table class="diff diff-contentalign-left" data-mw="interface">${result && diffMarker}<tbody>${showTitle ? diffTitle : ''}${result || '<tr><td colspan="2" class="diff-notice"><div class="mw-diff-empty">（没有差异）</div></td></tr>'}</tbody></table>`;
 };
 
 export default compare;
