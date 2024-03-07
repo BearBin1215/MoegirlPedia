718cc5bf90c63f8fccf365ef480fb296c4d4afe1
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-03-08 00:38:57 +0800

    refactor:

--- src/@types/global.ts
+++
@@ -1,7 +0,0 @@
-import type { Options } from 'html2canvas';
-
-declare global {
-  interface Window {
-    html2canvas: (element: HTMLElement, options?: Options) => Promise<HTMLCanvasElement>;
-  }
-}
--- src/gadgets/LyricStyleToggle/index.ts
+++ src/gadgets/LyricStyleToggle/index.ts
@@ -1,3 +1,7 @@
+interface HTMLElementWithAllStyles extends HTMLElement {
+  allStyles?: string;
+}
+
 $(() => (async () => {
   if (!document.getElementsByClassName('Lyrics')[0] && !(document.getElementsByClassName('poem')[0] && $('#catlinks').text().match(/音[乐樂]作品/))) {
     return;
@@ -12,22 +16,22 @@ $(() => (async () => {
     $(document.body).toggleClass('lyric-style-toggle-on');
 
     if (document.getElementsByClassName('lyric-style-toggle-on')[0]) {
-      $('.Lyrics-original').each(function () {
+      $('.Lyrics-original').each(function (this: HTMLElementWithAllStyles) {
         this.allStyles = $(this).attr('style');
         $(this).attr('style', 'color:#000');
       });
-      $('.Lyrics-translated').each(function () {
+      $('.Lyrics-translated').each(function (this: HTMLElementWithAllStyles) {
         this.allStyles = $(this).attr('style');
         $(this).attr('style', 'color:#656565');
       });
-      $('.Lyrics, .Lyrics span:not(.mw-collapsible), .poem, .poem span').each(function () {
+      $('.Lyrics, .Lyrics span:not(.mw-collapsible), .poem, .poem span').each(function (this: HTMLElementWithAllStyles) {
         this.allStyles = $(this).attr('style');
         $(this).attr('style', '');
       });
       btn.text('恢复样式');
     } else {
-      $('.Lyrics, .Lyrics-original, .Lyrics-translated, .Lyrics span:not(.mw-collapsible), .poem, .poem span').each(function () {
-        $(this).attr('style', this.allStyles);
+      $('.Lyrics, .Lyrics-original, .Lyrics-translated, .Lyrics span:not(.mw-collapsible), .poem, .poem span').each(function (this: HTMLElementWithAllStyles) {
+        $(this).attr('style', this.allStyles!);
       });
       btn.text('歌词除色');
     }
