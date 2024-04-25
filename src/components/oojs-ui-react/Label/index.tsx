9a49d3ec15579e952899682fdffd74caa609d208
BearBin <20369414+BearBin1215@users.noreply.github.com>
2024-04-25 17:11:22 +0800

    fix: 类型

--- src/components/oojs-ui-react/Label/index.tsx
+++ src/components/oojs-ui-react/Label/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
 import LabelBase from './Base';
-import type { HTMLAttributes, FunctionComponent } from 'react';
+import type { FunctionComponent } from 'react';
 import type { WidgetProps } from '../props';
 import type { LabelElement } from '../mixin';
-import type { LabelBaseProps } from './Base';
 
 export interface LabelProps extends
-  Omit<HTMLAttributes<HTMLSpanElement>, 'className'>,
   WidgetProps<HTMLSpanElement>,
-  LabelBaseProps,
   LabelElement { }
 
 const Label: FunctionComponent<LabelProps> = ({
