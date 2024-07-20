3bffb8a96e59f2d721fd94b22d8a98ce2b08dbe3
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-07-20 14:59:36 +0800

    build(deps): upgrade

--- scripts/src/Synchronize.js
+++ scripts/src/Synchronize.js
@@ -13,6 +13,7 @@ const blackListWindowsFunc = [
   'decodeURIComponent',
   'encodeURIComponent',
   'alert',
+  'confirm',
   'eval',
 ];
 
@@ -25,6 +26,7 @@ const blackListDOMFunc = [
 const blackListAllFunc = [
   'charCodeAt',
   'alert',
+  'confirm',
 ];
 
 /** 最近一次提交所修改的文件 */
@@ -73,14 +75,14 @@ if (!list.length) {
     const title = `${config.sync.pagePath}${item}.js`;
     /** 绕开被WAF的函数 */
     const source = (await fs.promises.readFile(`${config.sync.localPath}${item}.min.js`, 'utf-8')).replace(
+      new RegExp(`.(${blackListAllFunc.join('|')})\\(`, 'g'),
+      (_match, p1) => `['${p1}'](`,
+    ).replace(
       new RegExp(`(${blackListWindowsFunc.join('|')})\\(`, 'g'),
       (_match, p1) => `window['${p1}'](`,
     ).replace(
       new RegExp(`document.(${blackListDOMFunc.join('|')})\\(`, 'g'),
       (_match, p1) => `document['${p1}'](`,
-    ).replace(
-      new RegExp(`.(${blackListAllFunc.join('|')})\\(`, 'g'),
-      (_match, p1) => `['${p1}'](`,
     );
     /** 加上页顶文档文本、nowiki标签，生成最后要提交到页面的源码 */
     const text = `var _addText = '{{Documentation|content=* 工具介绍见[[User:BearBin/js#${item}]]。\\n* 源代码见[https://github.com/BearBin1215/MoegirlPedia/blob/master/src/gadgets/${item} GitHub]。}}';\n\n// <nowiki>\n\n${source}\n\n// </nowiki>`;
