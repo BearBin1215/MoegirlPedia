68f9b40122ea4f95dc4c0e57503a94973b76758b
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-07-21 15:16:53 +0800

    perf(CtrlS):

--- src/gadgets/CtrlS/index.ts
+++ src/gadgets/CtrlS/index.ts
@@ -1,34 +1,35 @@
 if (['edit', 'submit'].includes(mw.config.get('wgAction'))) {
-  const si = setInterval;
   // 编辑页Ctrl+S保存，Ctrl+Shift+S小编辑保存
   window.addEventListener('keydown', (event) => {
     if (event.ctrlKey) {
-      if (event.key.toLowerCase() === 's') {
-        // Ctrl+S保存
-        event.preventDefault();
-        if (event.shiftKey) {
-          // Ctrl+Shift+S切换小编辑后保存
-          document.getElementById('wpMinoredit')!.click();
-        }
-        document.getElementById('wpSave')!.click();
-      } else if (event.key === 'V') {
-        // Ctrl+Shift+V预览
-        event.preventDefault();
-        // 模块页“预览使用本模板的页面”
-        if (mw.config.get('wgCodeEditorCurrentLanguage') === 'lua') {
-          (document.querySelector('#wpTemplateSandboxPreview input') as HTMLInputElement)!.click();
-        }
-        document.getElementById('wpPreview')!.click();
-      } else if (event.key === 'D') {
-        // Ctrl+Shift+D查看差异
-        event.preventDefault();
-        document.getElementById('wpDiff')!.click();
+      event.preventDefault();
+      switch (event.key) {
+        case 's':
+        case 'S':
+          // Ctrl+S保存
+          if (event.shiftKey) {
+            // Ctrl+Shift+S切换小编辑后保存
+            document.getElementById('wpMinoredit')!.click();
+          }
+          document.getElementById('wpSave')!.click();
+          break;
+        case 'V': // Ctrl+Shift+V预览
+          event.preventDefault();
+          // 模块页“预览使用本模板的页面”
+          if (mw.config.get('wgCodeEditorCurrentLanguage') === 'lua') {
+            (document.querySelector('#wpTemplateSandboxPreview input') as HTMLInputElement)!.click();
+          }
+          document.getElementById('wpPreview')!.click();
+          break;
+        case 'D': // Ctrl+Shift+D查看差异
+          event.preventDefault();
+          document.getElementById('wpDiff')!.click();
       }
     }
   });
 
   // 在编辑框中时按tab跳转到摘要输入框而非快捷插入
-  const itv = si(() => {
+  const itv = setInterval(() => {
     const lineWrapping = document.querySelector<HTMLDivElement>('.cm-lineWrapping');
     if (lineWrapping) {
       lineWrapping.tabIndex = 1;
