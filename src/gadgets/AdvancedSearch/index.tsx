08a9ba1f726b8e72361c480473828c0f29183d9d
BearBin <20369414+BearBin1215@users.noreply.github.com>
2024-08-07 20:06:45 +0800

    chore:

--- src/gadgets/AdvancedSearch/index.tsx
+++ src/gadgets/AdvancedSearch/index.tsx
@@ -1,5 +1,14 @@
-(() => {
-  $('#simpleSearch').append(
-    $('<button>高级</button>'),
-  );
-})();
+$(() => {
+  $('#searchform').append(
+    $('<button id="advanced-search">高级</button>').css({
+      border: 0,
+      background: 'none',
+    }).on('click', (ev) => {
+      ev.preventDefault();
+    }),
+  ).css({
+    display: 'flex',
+    alignItems: 'baseline',
+    marginTop: '0',
+  });
+});
