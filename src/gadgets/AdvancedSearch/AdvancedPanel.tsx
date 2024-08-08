34516e4ab556d53af38821278e0f33a35dc20146
BearBin <20369414+BearBin1215@users.noreply.github.com>
2024-08-08 10:28:12 +0800

    feat(AdvancedSearch): 搜索类型

--- src/gadgets/AdvancedSearch/AdvancedPanel.tsx
+++ src/gadgets/AdvancedSearch/AdvancedPanel.tsx
@@ -4,8 +4,34 @@ import { Button } from 'oojs-ui-react';
 import './index.less';
 import type { FC } from 'react';
 
+/** 可用的搜索代码 */
+type SearchCode =
+  'insource' | // 源码包含
+  'prefix' | // 前缀
+  'intitle' | // 标题包含
+  'incategory' | // 属于分类
+  'linksto' | // 链接到
+  'hastemplate' | // 嵌入模板
+  'contentmodel' | // 内容模型
+  'subpageof' | // 从属子页面
+  'filetype' | // 文件类型
+  'filemime' | // 文件MIME
+  'filesize' | // 文件大小
+  'filewidth' | // 文件宽度
+  'fileheight' | // 文件高度
+  'fileres' | // 文件分辨率
+  'filebits' | // 文件色深
+  undefined;
+
+/** 搜索条件 */
+interface Condition {
+  type: SearchCode;
+  text: string;
+}
+
 const AdvancedPanel: FC = () => {
   const [show, setShow] = useState(false);
+  const [conditions, setConditions] = useState<Condition[]>([]);
 
   /** 展开或隐藏面板 */
   const toggle = () => {
