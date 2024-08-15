21f1a61caa2a44367b41782c94daa88244c7c870
BearBin <20369414+BearBin1215@users.noreply.github.com>
2024-08-15 16:33:45 +0800

    chore(oojs-ui-react): 未去除属性

--- src/components/oojs-ui-react/src/components/widgets/OutlineSelect/index.tsx
+++ src/components/oojs-ui-react/src/components/widgets/OutlineSelect/index.tsx
@@ -1,14 +1,9 @@
-import React, { forwardRef, type ReactElement } from 'react';
+import React, { forwardRef } from 'react';
 import classNames from 'classnames';
 import Select, { type SelectProps } from '../Select';
-import type { OutlineOptionProps } from '../OutlineOption';
 import type { ElementRef } from '../../../types/ref';
 
 export type OutlineSelectProps = SelectProps;
-
-export interface OptionElement extends ReactElement<OutlineOptionProps> {
-  children?: OptionElement | OptionElement[];
-}
 
 const OutlineSelect = forwardRef<ElementRef<HTMLDivElement>, OutlineSelectProps>(({
   className,
