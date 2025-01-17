6bc6113c01ac4c633e6ad4bceccaa01cc621a4d1
BearBin <20369414+BearBin1215@users.noreply.github.com>
2025-01-17 15:15:32 +0800

    perf:

--- src/oddments/CollapsedModerationStatus.js
+++ src/oddments/CollapsedModerationStatus.js
@@ -1,9 +1,9 @@
 /** @description 最近更改列表中，将被合并折叠的多条记录中最新记录状态作为整个状态显示 */
 $('.mw-collapsible.mw-changeslist-line').each(function (_, ele) {
   var $modIcon = $(ele).find('.mod-status-icon').first();
-  $modIcon[0].previousSibling.remove();
   if ($modIcon[0].previousSibling.textContent.includes('（')) {
     $modIcon[0].nextSibling.remove();
   }
+  $modIcon[0].previousSibling.remove();
   $(ele).find('tbody>tr:first-child .mw-changeslist-line-inner').prepend($modIcon);
 });
