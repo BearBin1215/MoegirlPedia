d2277e4e2b2b479894a88967ba4659ab16771e15
BearBin <20369414+BearBin1215@users.noreply.github.com>
2025-01-17 15:18:00 +0800

    fix: type error

--- src/oddments/CollapsedModerationStatus.js
+++ src/oddments/CollapsedModerationStatus.js
@@ -1,6 +1,9 @@
 /** @description 最近更改列表中，将被合并折叠的多条记录中最新记录状态作为整个状态显示 */
 $('.mw-collapsible.mw-changeslist-line').each(function (_, ele) {
   var $modIcon = $(ele).find('.mod-status-icon').first();
+  if (!$modIcon[0]) {
+    return;
+  }
   if ($modIcon[0].previousSibling.textContent.includes('（')) {
     $modIcon[0].nextSibling.remove();
   }
