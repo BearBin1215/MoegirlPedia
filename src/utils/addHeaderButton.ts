be629aa329ca6902ad845f86f2fd375c76bed0e4
BearBin <20369414+BearBin1215@users.noreply.github.com>
2025-05-14 16:52:12 +0800

    feat(utils): 标题按钮微调

--- src/utils/addHeaderButton.ts
+++ src/utils/addHeaderButton.ts
@@ -1,10 +1,13 @@
 import styles from './headerButton.less' assert { type: 'string '};
 
+/** 在页面标题尾部增加按钮 */
 const addHeaderButton = ({
   text = '',
 }) => {
   // 添加样式
-  $(document.head).append($(`<style id="bearbintool-headerbutton-style">${styles}</style>`));
+  if (!document.getElementById('bearbintool-headerbutton-style')) {
+    $(document.head).append(`<style id="bearbintool-headerbutton-style">${styles}</style>`);
+  }
 
   /** 标题按钮区域 */
   let $buttonZone = $('.bearbintool-headerbutton-wrapper');
@@ -14,7 +17,7 @@ const addHeaderButton = ({
     $('#firstHeading').append($buttonZone);
   }
 
-  const $button = $(`<button class="bearbintool-headerbutton">${text}</button>`);
+  const $button = $(`<button class="bearbintool-headerbutton">${text}</button>`) as JQuery<HTMLButtonElement>;
   $buttonZone.append($button);
 
   return $button;
