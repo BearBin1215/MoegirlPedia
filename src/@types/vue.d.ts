642a7a71ef20806cfc91cfca75294b91ec4e997b
BearBin <20369414+BearBin1215@users.noreply.github.com>
2025-07-24 09:49:40 +0800

    fix(types): 按eslint调整类型声明

--- src/@types/vue.d.ts
+++ src/@types/vue.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
+  import { type DefineComponent } from 'vue';
+  const component: DefineComponent<object, object, any>;
   export default component;
 }
